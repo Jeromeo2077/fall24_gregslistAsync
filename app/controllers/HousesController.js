903183f19d4867bfba1316e75933ed0407c236d0
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-03 16:21:13 -0600

    Started on the getHouses function which will receive data from the houses API

--- app/controllers/HousesController.js
+++ app/controllers/HousesController.js
@@ -1,6 +1,18 @@
+import { housesService } from "../services/HousesService.js";
+import { Pop } from "../utils/Pop.js";
+
 export class HousesController {
   constructor() {
     console.log('HousesController Loaded');
 
+  }
+
+  async getHouses() {
+    try {
+      await housesService.getHouses()
+    } catch (error) {
+      Pop.error(error)
+      console.error(error)
+    }
   }
 }
--- app/services/HousesService.js
+++ app/services/HousesService.js
@@ -1,4 +1,12 @@
+import { api } from "./AxiosService.js"
+
 class HousesService {
+
+  async getHouses() {
+    const response = await api.get('api/houses')
+    console.log('Houses received from API: ', response.data);
+
+  }
 
 }
 
